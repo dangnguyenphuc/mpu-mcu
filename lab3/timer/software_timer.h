1dd6aeb6f58ad504f5bc91ce9e613b3fe099d4cf
Dang Nguyen <phucdang91thcsdl@gmail.com>
2022-10-30 14:47:37 +0700

    Add timer to lab3

---
+++ lab3/timer/software_timer.h
@@ -0,0 +1,37 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Sep 24, 2022
+ *      Author: phucd
+ */
+
+#ifndef __SOFTWARE_TIMER_
+#define __SOFTWARE_TIMER_
+
+/*Timer 1*/
+extern int timer1_flag ;
+void setTimer1(int duration);
+void clearTimer1();
+
+/*Timer 2*/
+extern int timer2_flag ;
+void setTimer2(int duration);
+void clearTimer2();
+/*Timer 3*/
+extern int timer3_flag ;
+void setTimer3(int duration);
+void clearTimer3();
+/*Timer 4*/
+extern int timer4_flag ;
+void setTimer4(int duration);
+void clearTimer4();
+/*Timer 5*/
+extern int timer5_flag ;
+void setTimer5(int duration);
+void clearTimer5();
+//Count down for all timers
+void TimeRun();
+
+
+
+#endif /* __SOFTWARE_TIMER_ */
