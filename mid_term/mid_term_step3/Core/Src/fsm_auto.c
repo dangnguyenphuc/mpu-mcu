50d2b05c7ac4c973f3d9edbb01e135da3657a1e1
Dang Nguyen <phucdang91thcsdl@gmail.com>
2022-11-05 14:33:12 +0700

    upload step3

---
+++ mid_term/mid_term_step3/Core/Src/fsm_auto.c
@@ -0,0 +1,93 @@
+/*
+ * fsm_auto.c
+ *
+ *  Created on: Nov 5, 2022
+ *      Author: phucd
+ */
+
+#include "fsm_auto.h"
+#include "timer_task.h"
+
+void fsm_auto_run(){
+	switch(status){
+
+			case INIT:
+				// TODO
+
+				clearLED();
+				counter = 0;
+				setTimer1(100);
+
+				// Switch State
+				status = AUTO_STATE;
+
+				break;
+
+			case AUTO_STATE:
+				// TODO
+
+				//display counter;
+				displayNum(counter);
+				// count counter;
+				count_down();
+
+
+
+				// Switch State
+
+				// just clicking
+					// If 1st button pressed then increasing
+				if( is_button_pressed(0) ){
+					counter += 1;
+					check_counter();
+					setTimer5(EXPIRED_BUTTON_DURATION);
+
+					status = INCREASE;
+				}
+					// If 2nd button pressed then decreasing
+				if( is_button_pressed(1) ){
+					counter -= 1;
+					check_counter();
+					setTimer5(EXPIRED_BUTTON_DURATION);
+
+					status = DECREASE;
+				}
+					// If 3rd button pressed then reset
+				if( is_button_pressed(2) ){
+					counter = 0;
+					setTimer5(EXPIRED_BUTTON_DURATION);
+
+					status = RESET_STATE;
+				}
+
+				// hold pressing
+					// If 1st button pressed then increasing
+				if( is_button_pressed_3s(0) ){
+					counter += 1;
+					check_counter();
+					setTimer5(EXPIRED_BUTTON_DURATION);
+
+					status = INCREASE;
+
+				}
+					// If 2nd button pressed then decreasing
+				if( is_button_pressed_3s(1) ){
+					counter -= 1;
+					check_counter();
+					setTimer5(EXPIRED_BUTTON_DURATION);
+
+					status = DECREASE;
+				}
+				if( is_button_pressed_3s(2) ){
+					counter = 0;
+					setTimer5(EXPIRED_BUTTON_DURATION);
+
+					status = RESET_STATE;
+				}
+
+				break;
+
+			default:
+				break;
+		}
+}
---
+++ mid_term/mid_term_step3/Debug/Core/Src/subdir.mk
@@ -0,0 +1,56 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/button.c \
+../Core/Src/fsm_auto.c \
+../Core/Src/fsm_simple_button.c \
+../Core/Src/global.c \
+../Core/Src/led.c \
+../Core/Src/main.c \
+../Core/Src/software_timer.c \
+../Core/Src/stm32f1xx_hal_msp.c \
+../Core/Src/stm32f1xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32f1xx.c \
+../Core/Src/timer_task.c 
+
+OBJS += \
+./Core/Src/button.o \
+./Core/Src/fsm_auto.o \
+./Core/Src/fsm_simple_button.o \
+./Core/Src/global.o \
+./Core/Src/led.o \
+./Core/Src/main.o \
+./Core/Src/software_timer.o \
+./Core/Src/stm32f1xx_hal_msp.o \
+./Core/Src/stm32f1xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32f1xx.o \
+./Core/Src/timer_task.o 
+
+C_DEPS += \
+./Core/Src/button.d \
+./Core/Src/fsm_auto.d \
+./Core/Src/fsm_simple_button.d \
+./Core/Src/global.d \
+./Core/Src/led.d \
+./Core/Src/main.d \
+./Core/Src/software_timer.d \
+./Core/Src/stm32f1xx_hal_msp.d \
+./Core/Src/stm32f1xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32f1xx.d \
+./Core/Src/timer_task.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103x6 -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
